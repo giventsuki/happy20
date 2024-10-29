fcf854ca616be79c4cced7ba1c42a3f9a654a33b
tsukinom1 <example@gmail.com>
2024-10-29 19:22:07 +0500

    Initial commit

---
+++ src/components/Slider/ImageSlider/Slides.js
@@ -0,0 +1,12 @@
+
+const slides = [
+    {src: '/image/myMindImages/first.jpg', alt: `image`},
+    {src: '/image/myMindImages/second.jpg', alt: `image`},
+    {src: '/image/myMindImages/third.jpg', alt: `image`},
+    {src: '/image/myMindImages/fourth.jpg', alt: `image`},
+    {src: '/image/myMindImages/fifth.jpg', alt: `image`},
+]
+
+
+export default slides;
+
